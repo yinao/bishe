4fee6de38cd5b170504599a435ca1395ac211208
yinao <1065669324@qq.com>
2016-04-03 10:42:36 +0800

    wps documents

--- Web/Action/IndexAction.class.php
+++ Web/Action/IndexAction.class.php
@@ -7,9 +7,9 @@ class IndexAction extends BaseAction{
 	public function run(){
 		//echo 'this is index';
 		$this->obj->caching=false;
-		$this->obj->assign('title','this is title');
-		$this->obj->assign('keyword','');
-		$this->obj->assign('description','');
+		$this->obj->assign('title','首页title');
+		$this->obj->assign('keyword','关键字');
+		$this->obj->assign('description','网站页面内容描述');
 		$this->obj->assign('content','这是首页显示');
 		$this->obj->display('index.html');
 	}
--- Web/templates_c/8583750c19ed3bd2a596e364149676ef7f60b8b6_0.file.head.html.php
+++ Web/templates_c/8583750c19ed3bd2a596e364149676ef7f60b8b6_0.file.head.html.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2016-04-02 23:06:22
+/* Smarty version 3.1.29, created on 2016-04-03 10:02:03
   from "/var/www/bishe/Web/templates/head.html" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_56ffdfee409de6_97303560',
+  'unifunc' => 'content_5700799b82eca6_16781695',
   'file_dependency' => 
   array (
     '8583750c19ed3bd2a596e364149676ef7f60b8b6' => 
     array (
       0 => '/var/www/bishe/Web/templates/head.html',
-      1 => 1459600341,
+      1 => 1459648920,
       2 => 'file',
     ),
   ),
@@ -19,15 +19,17 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_56ffdfee409de6_97303560 ($_smarty_tpl) {
+function content_5700799b82eca6_16781695 ($_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
 </title>
-	<meta name="keyword" content=""/>
-	<meta name="description" content=""/>
+	<meta name="keyword" content="<?php echo $_smarty_tpl->tpl_vars['keyword']->value;?>
+"/>
+	<meta name="description" content="<?php echo $_smarty_tpl->tpl_vars['description']->value;?>
+"/>
 	<style href=""></style>
 </head>
 <body><?php }
